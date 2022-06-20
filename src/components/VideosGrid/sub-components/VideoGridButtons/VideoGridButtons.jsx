25141d47f4f899a13710cdd67f60d21ceebf13f4
avipj <avinashprajapati914@gmail.com>
2022-06-20 22:34:43 +0530

    added homepage buttons for navigation

---
+++ src/components/VideosGrid/sub-components/VideoGridButtons/VideoGridButtons.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+export const VideoGridButtons = () => (
+  <div className="homepage-buttons flex-base ">
+    <button cursor="pointer" className="homepage-button" type="button">
+      All
+    </button>
+    <button cursor="pointer" className="homepage-button" type="button">
+      TED
+    </button>
+    <button cursor="pointer" className="homepage-button" type="button">
+      Life
+    </button>
+    <button cursor="pointer" className="homepage-button" type="button">
+      Science
+    </button>
+    <button cursor="pointer" className="homepage-button" type="button">
+      Learning
+    </button>
+    <button cursor="pointer" className="homepage-button" type="button">
+      Creativity
+    </button>
+    <button cursor="pointer" className="homepage-button" type="button">
+      Work
+    </button>
+    <button cursor="pointer" className="homepage-button" type="button">
+      Kurzgesagt
+    </button>
+    <button cursor="pointer" className="homepage-button" type="button">
+      Psychology
+    </button>
+  </div>
+);
