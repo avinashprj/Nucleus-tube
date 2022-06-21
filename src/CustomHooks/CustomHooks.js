dc9193dd816330043419496a66afdc0bfa2b27c0
avipj <avinashprajapati914@gmail.com>
2022-06-21 16:15:11 +0530

    added useInput hook for inputs

--- src/CustomHooks/CustomHooks.js
+++ src/CustomHooks/CustomHooks.js
@@ -14,4 +14,16 @@ function useCloseOnClickOutside(ref, handler = '') {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [ref]);
 }
-export { useCloseOnClickOutside };
+const useInput = (state) => {
+  const [inputState, setInputState] = React.useState(state);
+  const inputUpdate = (e) => {
+    const inpValue = e.target.value;
+    setInputState({
+      ...inputState,
+      [e.target.name]: inpValue,
+    });
+  };
+  return { inputState, inputUpdate };
+};
+
+export { useCloseOnClickOutside, useInput };
