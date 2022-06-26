51993fd027143fdd56aa3c63d10b58b2448e12ac
avipj <avinashprajapati914@gmail.com>
2022-06-26 23:03:46 +0530

    misc

--- src/components/VideosGrid/VideosGrid.jsx
+++ src/components/VideosGrid/VideosGrid.jsx
@@ -1,19 +1,53 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { VideoGridButtons } from './sub-components/VideoGridButtons/VideoGridButtons';
 import { VideoCard } from '../VideoCard/VideoCard';
+import {
+  useGetCategoriesQuery,
+  useGetVideosQuery,
+} from '../../features/api/fetchVideos/fetchVideosSlice';
+import { setCategories, setVideos } from '../../features/videos/videosSlice';
+import { Loader } from '../Loader/Loader';
+import { getCategorizedData, shuffleArray } from '../../utils/utils';
 
-export const VideosGrid = ({ playlistModal, setPlaylistModal }) => (
-  <div className="flex-base flex-column container">
-    <VideoGridButtons />
-    <div className="flex-base">
-      <div className="outer-grid video-outer-grid">
-        <div className="video-grid">
-          <VideoCard
-            playlistModal={playlistModal}
-            setPlaylistModal={setPlaylistModal}
-          />
+export const VideosGrid = ({ playlistModal, setPlaylistModal }) => {
+  const { videos, categories, category } = useSelector((store) => store.videos);
+  const dispatch = useDispatch();
+  const { authToken } = useSelector((store) => store.authentication);
+  const { isLoading, data } = useGetVideosQuery();
+  const { data: categoryData } = useGetCategoriesQuery();
+  React.useEffect(() => {
+    if (data) {
+      dispatch(setVideos(data));
+    }
+    if (categoryData) {
+      dispatch(setCategories(categoryData));
+    }
+  }, [categoryData, data, dispatch, videos]);
+
+  // let newArray = [...videos];
+  // newArray = shuffleArray(newArray);
+  const categorizedArray = getCategorizedData(videos, category);
+
+  return (
+    <div className="flex-base flex-column container">
+      <VideoGridButtons categories={categories} />
+      <div className="flex-base">
+        <div className="outer-grid video-outer-grid">
+          {videos.length === 0 && <Loader />}
+          <div className="video-grid">
+            {videos &&
+              categorizedArray.map((video) => (
+                <VideoCard
+                  singleVideo={video}
+                  key={video._id}
+                  playlistModal={playlistModal}
+                  setPlaylistModal={setPlaylistModal}
+                />
+              ))}
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
 import './styles/index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
 import { makeServer } from './server';
+import { store, persistor } from './store/store';
+import { ThemeContextProvider } from './context/ThemeContext/ThemeContext';
 
 // Call make Server
 makeServer();
@@ -12,7 +16,13 @@ const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <ThemeContextProvider>
+            <App />
+          </ThemeContextProvider>
+        </PersistGate>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
