7c114d578fd8a2861f428838384dffa8649b8a48
avipj <avinashprajapati914@gmail.com>
2022-06-27 00:41:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,20 +8,32 @@ import { useScrollToTop } from './CustomHooks/CustomHooks';
 import { useGetLikesQuery } from './features/api/likesApi/likesSliceApi';
 import { setLikes } from './features/likes/likesSlice';
 import { useThemeContext } from './context/ThemeContext/ThemeContext';
+import { useGetHistoryQuery } from './features/api/historyApi/historySliceApi';
+import { setHistory } from './features/history/historySlice';
+import { useGetWatchLaterQuery } from './features/api/watchLaterApi/watchLaterApi';
+import { setWatchLater } from './features/watchLater/watchLaterSlice';
 
 function App() {
   useScrollToTop();
   const [playlistModal, setPlaylistModal] = React.useState(false);
   const { authToken } = useSelector((store) => store.authentication);
   const [skip, setSkip] = React.useState(true);
-  const { data } = useGetLikesQuery(authToken, { skip });
+  const { data: likesData } = useGetLikesQuery(authToken, { skip });
+  const { data: historyData } = useGetHistoryQuery(authToken, { skip });
+  const { data: watchLaterData } = useGetWatchLaterQuery(authToken, { skip });
 
   const dispatch = useDispatch();
   React.useEffect(() => {
-    if (data) {
-      dispatch(setLikes(data.likes));
+    if (likesData) {
+      dispatch(setLikes(likesData.likes));
     }
-  }, [data, dispatch, setSkip]);
+    if (historyData) {
+      dispatch(setHistory(historyData.history));
+    }
+    if (watchLaterData) {
+      dispatch(setWatchLater(historyData.watchlater));
+    }
+  }, [dispatch, historyData, likesData, setSkip, watchLaterData]);
   const [colorState] = useThemeContext();
   return (
     <div className="app" data-theme={colorState}>
